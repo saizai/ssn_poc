a9400e449bcc1146112b0e85ddb6849c05d6033a
Sai <github@saizai.com>
2015-03-06 11:39:02 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,99 @@
+source 'https://rubygems.org'
+
+platform(:rbx) do
+  gem 'rubysl'
+  gem 'ffi'
+  # Use Psych as the YAML engine, instead of Syck, so serialized data can be read safely
+  #  from different rubies (see http://git.io/uuLVag)
+  gem 'psych'
+end
+
+gem 'rails'#, '>= 4.0.1'
+gem 'rake'#, '>= 0.9.2.2'
+# gem 'rack'#, '>= 1.4.1'
+# Use mysql as the database for Active Record
+gem 'mysql2'
+# Use SCSS for stylesheets
+gem 'sass-rails'#, '~> 5.0'
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier'#, '>= 1.3.0'
+# Use CoffeeScript for .coffee assets and views
+gem 'coffee-rails'#, '~> 4.1.0'
+gem 'execjs'#, '>= 1.3.0' # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+gem 'therubyracer', platforms: :ruby
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'#, '>= 3.0.0'
+gem 'jquery-ui-rails'#, '>= 5.0.0'
+gem 'jquery-turbolinks'
+gem 'turbolinks' # makes links load faster; see https://github.com/rails/turbolinks
+# gem 'jquery_datepicker' # rails 4 incompatible 2013-06-29
+gem 'bettertabs'
+
+gem 'json'#, '>= 1.6.6'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder'#, '~> 2.0'
+
+# bundle exec rake doc:rails generates the API under doc/api.
+gem 'sdoc'#, '~> 0.4.0', group: :doc
+
+
+group :development do
+  gem 'capistrano', require: false#, '>= 3.0.1'
+  gem 'capistrano-rails', require: false
+  gem 'capistrano-bundler', require: false
+  # gem 'capistrano-rvm', require: false
+  # gem 'capistrano-puma', require: false # use service instead
+
+  # gem 'capistrano-ci' # not cap3 compatible yet https://github.com/railsware/capistrano-ci/pull/4
+  gem 'term-ansicolor'
+  platform(:mri) do # Ruby 2.0+ required.
+    gem 'byebug'
+    gem 'pry-byebug'
+  end
+  platform(:rbx) do
+    gem 'rubinius-compiler'
+    gem 'rubinius-debugger'
+  end
+
+  gem 'pry'
+  gem 'pry-rails'
+  gem 'pry-doc'
+  gem 'pry-git'
+  gem 'awesome_print'
+
+  gem 'pry-rescue'
+  gem 'pry-stack_explorer'
+
+  gem "better_errors"
+  gem "binding_of_caller"
+end
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+group :development, :test do
+  platform(:mri) { gem 'ruby-prof' }
+  gem 'webrat'#, '>= 0.7.3'
+  gem "brakeman", require: false  # Rails security scanner
+  gem 'rubocop', require: false
+  gem 'bundler-audit', require: false
+  # gem 'rspec-rails'#, '~> 3.0.0.beta'
+  gem 'spring'
+end
+
+group :test do
+  # gem 'cucumber-rails', :require => false
+  gem 'database_cleaner'
+end
+
+gem 'Empact-activerecord-import'#, '>= 0.4.1' # zdennis hasn't yet imported the import profiling fix; this is a bugfix tracking fork
+
+gem 'groupdate' # https://github.com/ankane/groupdate
+
+gem "strip_attributes"#, ">= 1.1.0"
+# gem 'client_side_validations' # rails 4 incompatible
+
+gem 'friendly_id'#, ">= 5.0.0"
+
+gem 'ssn_validator'
---
+++ config/deploy.rb
@@ -0,0 +1,103 @@
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :application, 'ssn_poc'  # Required
+
+set :ci_client, 'travis'
+set :ci_repository, 'saizai/ssn_poc'
+
+set :scm, :git # :git, :darcs, :subversion, :cvs
+set :repo_url, 'git://github.com/saizai/ssn_poc.git'
+set :branch, 'master'
+set :git_enable_submodules, 1
+
+# set :gateway, "gate.host.com"  # default to no gateway
+set :runner, 'saizai_ssn'
+set :group, 'pg886808'
+set :deploy_to, '/home/saizai_ssn/ssn.s.ai/' # must be path from root
+set :deploy_via, :remote_cache
+
+set :rails_env, 'production'
+
+set :ssh_options,
+    user:          'saizai_ssn',
+    compression:   false,
+    # keys:  %w(~/.ssh/myl_deploy),
+    forward_agent: true # make sure you have an SSH agent running locally
+# auth_methods: %w(password)
+# port: 25
+
+server 'ssn.s.ai', roles: [:web, :app, :db]
+
+# set :workers,  '*' => 2
+
+# Uncomment this line if your workers need access to the Rails environment:
+# set :resque_environment_task, true
+
+# set :format, :pretty
+# set :log_level, :debug
+set :pty, true  # turning on pty allows resque workers to be started without making capistrano hang
+
+# set :linked_files, %w{config/database.yml}
+set :linked_dirs,  %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system
+                      public/assets public/files db/data config/keys)
+
+set :keep_releases, 15
+
+# not cap3 compatible yet https://github.com/railsware/capistrano-ci/pull/4
+# before :deploy, "ci:verify"
+
+namespace :deploy do
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      within fetch(:release_path) do
+        execute :touch, "tmp/restart.txt"
+      end
+    end
+  end
+
+  desc "Set permissions on shared folders"
+  task :set_permissions do
+    on roles(:web) do
+      # execute "chmod -R u=rwX,g=rX,o= #{shared_path}/"
+      # execute "chmod o=rX #{shared_path}/"
+      # execute "chmod -R o=rX #{shared_path}/public/"
+
+      ['bin', 'bundle', 'config', 'db', 'log', 'tmp', 'tmp/cache', 'tmp/pids', 'vendor', 'vendor/bundle'].each do |i|
+        execute "chmod 2751 #{shared_path}/#{i}"
+      end
+      ['tmp/sockets', 'tmp/sockets/*.sock'].each do |i|
+        execute "chmod 2770 #{shared_path}/#{i}"
+      end
+      execute "chmod 2755 #{shared_path}/public"
+    end
+  end
+
+  before :finishing, 'deploy:set_permissions'
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+  # after :finishing, 'airbrake:deploy'
+
+  # # https://github.com/capistrano/capistrano/issues/478#issuecomment-24983528
+  # namespace :assets do
+  #   task :update_asset_mtimes, :roles => lambda { assets_role },
+  #                              :except => { :no_release => true } do
+  #   end
+  # end
+  # set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
+
+  task :down do
+    on roles(:app) do
+      execute :mv, "#{release_path}/public/maintenance_standby.html",
+              "#{release_path}/public/maintenance.html"
+    end
+  end
+  task :up do
+    on roles(:app) do
+      execute :mv, "#{release_path}/public/maintenance.html",
+              "#{release_path}/public/maintenance_standby.html"
+    end
+  end
+end
+
+require './config/boot'
